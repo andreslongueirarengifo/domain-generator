fff889d605149fe4b6b9a3526de578b33007e1ce
Andres Longueira <andreslongueirarengifo@gmail.com>
2022-11-18 18:49:37 +0000

    First commmit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let dom = "";
+
+  pronoun.forEach(pn => {
+    adj.forEach(ad => {
+      noun.forEach(n => {
+        dom = `${pn}${ad}${n}`;
+        console.log(dom);
+      });
+    });
+  });
 };
